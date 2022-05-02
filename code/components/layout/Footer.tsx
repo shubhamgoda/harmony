7a7a163a35253e4c7978b23af66cc8b92f479fc6
Nikhil Kundu <nk553@cornell.edu>
2022-05-01 22:43:53 -0400

    Commit name

--- code/components/layout/Footer.tsx
+++ code/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <footer>
     <hr />
-    <span>Created by Nikhil Kundu (nk553)</span>
+    <span>Created by Nikhil Kundu and Shubham Goda (nk553, svg24)</span>
   </footer>
 );
 
